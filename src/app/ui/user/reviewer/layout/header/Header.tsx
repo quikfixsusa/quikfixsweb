2adf3a5a551fa12307da8488370e1a5c78d65a03
kikeai <lg5967133@gmail.com>
2024-10-25 10:29:00 -0400

    add alert to sign out

--- src/app/ui/user/reviewer/layout/header/Header.tsx
+++ src/app/ui/user/reviewer/layout/header/Header.tsx
@@ -2,20 +2,42 @@ import BellIcon from '@/app/components/svg/icons/BellIcon';
 import LogoutIcon from '@/app/components/svg/icons/LogoutIcon';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import Swal from 'sweetalert2';
 
 import SearchInput from './SearchInput';
 
 export default function Header() {
   const router = useRouter();
-  const handleLogout = () => {
-    axios
-      .get('/api/logout', { withCredentials: true })
-      .then(() => {
-        router.push('/auth/login');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+  const handleLogout = async () => {
+    await Swal.fire({
+      html: `
+        <div class="flex flex-col items-start w-full">
+          <p class="text-2xl text-start font-bold text-black">Sing out</p>
+          <p class="text text-start text-base text-gray-600">Are you sure you want sign out?</p>
+        </div>
+      `,
+      showCancelButton: true,
+      showCloseButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#263AFF',
+      cancelButtonText: 'Cancel',
+      confirmButtonText: 'Sign out',
+      showLoaderOnConfirm: true,
+      allowOutsideClick: () => !Swal.isLoading(),
+      preConfirm: async () => {
+        try {
+          const response = await axios.get('/api/logout', { withCredentials: true });
+          if (response.status === 202) {
+            router.push('/auth/login');
+          }
+        } catch (error) {
+          Swal.showValidationMessage(`
+            Cannot sign out. Try again.
+          `);
+        }
+      },
+    });
   };
   return (
     <header className="flex w-full items-center justify-between border-b border-b-gray-300 p-4">
