037d7b7a190d50116f72e80f6b3ee464fe2fa924
kikeai <lg5967133@gmail.com>
2024-10-25 11:02:55 -0400

    add in review cases in dashboard

--- src/app/ui/user/reviewer/myCases/Cards.tsx
+++ src/app/ui/user/reviewer/myCases/Cards.tsx
@@ -5,11 +5,11 @@ import { User } from '@/app/lib/definitions';
 import Card from './Card';
 import CardSkeleton from './CardSkeleton';
 
-export default function Cards({ users }: { users: User[] }) {
+export default function Cards({ users, home }: { users: User[]; home?: boolean }) {
   const { loadingInProgress } = useReviewerContext();
-  const loadingSkeleton = [1, 2, 3, 4, 5, 6];
+  const loadingSkeleton = home ? [1, 2, 3] : [1, 2, 3, 4, 5, 6];
   return (
-    <div className="flex w-full flex-wrap gap-4 px-8 py-4">
+    <div className={`flex w-full flex-wrap gap-4 ${home ? '' : 'px-8 py-4'}`}>
       {users.length === 0 && !loadingInProgress && <h2 className="text-center font-medium">No cases</h2>}
       {users.length > 0 && !loadingInProgress && users.map((user) => <Card key={user.id} userData={user} />)}
       {loadingInProgress && loadingSkeleton.map((item) => <CardSkeleton key={item} />)}
